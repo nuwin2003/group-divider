9b1804ce41f647a7dda4a5b36b0b2511efbf916c
Nuwin Vinwath <131539550+nuwin2003@users.noreply.github.com>
2023-11-24 00:37:45 +0530

    Add group divide function step 01

--- lib/group_lists.dart
+++ lib/group_lists.dart
@@ -11,8 +11,45 @@ class GroupLists extends StatefulWidget {
 }
 
 class _GroupListsState extends State<GroupLists> {
+  @override
+  void initState() {
+    super.initState();
+
+    divideToGroups();
+  }
+
   var numberOfGroups = 0;
   var remainingMembers = 0;
+
+  List<List<String>> groups = [];
+
+  void divideGroups(List<String> group) {
+    setState(() {
+      groups.add(group);
+      print(groups);
+    });
+  }
+
+  void divideToGroups() {
+    setState(() {
+      if (widget.memberList.length % 5 == 0) {
+        numberOfGroups = (widget.memberList.length / 5).toInt();
+
+        //Outer loop iterate for number of groups
+        for (int i = 0; i < widget.memberList.length; i += 5) {
+          List<String> group = [];
+
+          for (int j = i; j < i + 5 && j < widget.memberList.length; j++) {
+            group.add(widget.memberList[j]);
+          }
+          divideGroups(group);
+          print(group);
+        }
+      }
+    });
+
+    //Need to add remainber below 3 and 4 also
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,17 +76,28 @@ class _GroupListsState extends State<GroupLists> {
                 ],
               ),
             ),
-            body: Container()));
+            body: SingleChildScrollView(
+                child: Column(children: [
+              for (int i = 0; i < groups.length; i++)
+                Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        title: Text("Group ${i + 1}", style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),textColor: Colors.blue,
+                      ),
+                      for (int j = 0; j < groups[i].length; j++)
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Text("${j + 1}) ${groups[i][j]}\n",style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                        )
+                    ],
+                  ),
+                )
+            ]))));
   }
 }
 
-void divideToGroups() {}
-
 //Maximum members for a group --> 6
-//Minimum members for agroup --> 4
+//Minimum members for a group --> 4
 
-//memberList.length/5
-
-
-
-//child: Text("${widget.memberList}")
